a9d29884e80b0404e627baec413a65a5abed53ed
SalauddinAyub <ayub35-265@diu.edu.bd>
2021-12-17 14:27:50 +0600

    list view,data summary,product variant data show

--- src/product/forms.py
+++ src/product/forms.py
@@ -1,6 +1,8 @@
+from django.views import generic
+from django.views.generic import ListView, CreateView, UpdateView
 from django.forms import forms, ModelForm, CharField, TextInput, Textarea, BooleanField, CheckboxInput
 
-from product.models import Variant
+from product.models import Variant, Product
 
 
 class VariantForm(ModelForm):
@@ -11,4 +13,14 @@ class VariantForm(ModelForm):
             'title': TextInput(attrs={'class': 'form-control'}),
             'description': Textarea(attrs={'class': 'form-control'}),
             'active': CheckboxInput(attrs={'class': 'form-check-input', 'id': 'active'})
+        }
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product        
+        fields = '__all__'
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'sku': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
         }
